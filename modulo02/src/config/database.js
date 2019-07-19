faa1ca6c44838bb90bc6a4fd09973f9c9bbda83e
Rodrigo Finger <rodrigo.finger@digitaldesk.com.br>
2019-07-19 17:38:44 -0300

    chore: Migrations básica configurada

---
+++ modulo02/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+	dialect: 'mysql',
+	host: 'localhost',
+	username: 'root',
+	password: 'root',
+	database: 'gobarber',
+	define: {
+		timestapms: true,
+		underscored: true,
+		underscoredAll: true,
+	},
+};
