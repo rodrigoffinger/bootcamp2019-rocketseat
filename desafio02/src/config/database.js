21736052b3cb8989f0e6a61f6a34f7925b42ae20
Rodrigo Finger <rodrigo.finger@digitaldesk.com.br>
2019-10-17 16:18:33 -0300

    Desafio 02 do bootcamp

---
+++ desafio02/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: 'root',
+  database: 'gympoint',
+  define: {
+    timestapms: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
