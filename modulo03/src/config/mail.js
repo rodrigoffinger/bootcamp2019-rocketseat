677a68dc6d157a322e9b26cf9c19aa52dc489653
Rodrigo Finger <rodrigo.finger@digitaldesk.com.br>
2019-10-28 14:33:38 -0300

    Envio de e-mails com templates

---
+++ modulo03/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '40be197dfc7059',
+    pass: 'd39d3ac3ee24aa',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
