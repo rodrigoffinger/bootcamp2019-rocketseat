855399fba6c11ed12dc23c1b5a7d11f37e21f938
Rodrigo Finger <rodrigo.finger@digitaldesk.com.br>
2019-10-28 15:36:23 -0300

    Fila de jobs com redis

---
+++ modulo03/src/app/Jobs/CancellationMail.js
@@ -0,0 +1,34 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
+
+class CancellationMail {
+  get key() {
+    return 'CancellationMail';
+  }
+
+  async handle({ data }) {
+    const { appointment } = data;
+
+    console.log('A fila de envio de e-mails executou');
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          parseISO(appointment.date),
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          {
+            locale: pt,
+          }
+        ),
+      },
+    });
+  }
+}
+
+export default new CancellationMail();
---
+++ modulo03/src/config/redis.js
@@ -0,0 +1,5 @@
+export default {
+  host: 'redis-18175.c15.us-east-1-2.ec2.cloud.redislabs.com',
+  port: 18175,
+  password: '5bKjKrZP8zR8vthxZFn9q2iEsjHdzb1L',
+};
